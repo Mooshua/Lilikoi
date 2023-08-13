66357d546ad59042427252db0fe826e3feeec0b2
Mooshua <43320783+Mooshua@users.noreply.github.com>
2023-08-13 13:29:19 -0700

    Change wrap validation behavior to use inner container return instead of container result

--- Lilikoi/Compiler/Mahogany/MahoganyValidator.cs
+++ Lilikoi/Compiler/Mahogany/MahoganyValidator.cs
@@ -1,7 +1,7 @@
 ﻿//       ========================
 //       Lilikoi::MahoganyValidator.cs
 //       (c) 2023. Distributed under the MIT License
-// 
+//
 // ->    Created: 23.12.2022
 // ->    Bumped: 06.02.2023
 //       ========================
@@ -62,7 +62,10 @@ internal static class MahoganyValidator
 
 	internal static void ValidateWrap(LkWrapBuilderAttribute attribute, MahoganyMethod method)
 	{
-		if (!ValidWrap(attribute, method.Input, method.Result, method.Mount))
-			throw new InvalidCastException($"Wrap '{attribute.GetType().FullName}'" + $"rejected input-output pair on method '{method.Entry.Name}'");
+		if (!ValidWrap(attribute, method.Input, method.Return, method.Mount))
+			throw new InvalidCastException($"Wrap '{attribute.GetType().FullName}' " +
+			                               $"rejected input-output pair on method '{method.Entry.Name}' " +
+			                               $"with input of {method.Input.FullName} " +
+			                               $"and inner container output of {method.Return.FullName} ");
 	}
 }
